207a21512cf3981bd366f96fe7c62afadd6bf6a1
Barbara Peric <b.peric10@gmail.com>
2024-02-28 11:44:06 +0000

    scroll trigger animation

--- app/page.tsx
+++ app/page.tsx
@@ -16,77 +16,29 @@ export default function Home() {
     const useIsomorphicLayoutEffect =
         typeof window !== 'undefined' ? useLayoutEffect : useEffect
 
-    // useEffect(() => {
-    //     const tl = gsap.timeline({
-    //         scrollTrigger: {
-    //             scrub: 1,
-    //             pin: true,
-    //             trigger: scrollPage.current,
-    //             start: '50% 50%',
-    //             endTrigger: '#pin-windmill-wrap',
-    //             end: 'bottom 50%',
-    //         },
-    //     })
-
-    // }, [])
-
     useIsomorphicLayoutEffect(() => {
-        //rendering context
         let ctx = gsap.context(() => {
-            ScrollTrigger.defaults({
-                markers: true,
-            })
             const tl = gsap.timeline({
-                scrollTrigger: 'scrollPage.current',
+                scrollTrigger: {
+                    scrub: 1,
+                    pin: true,
+                    trigger: '#pin-windmill',
+                    start: '50% 50%',
+                    endTrigger: '#pin-windmill-wrap',
+                    end: 'bottom 50%',
+                },
             })
 
             tl.to('#pin-windmill-svg', {
-                rotateZ: 400,
+                rotateZ: 900,
             })
-
-            //1. SCROLLTRIGGER
-            // timeline.to('.spin_text', {
-            //     duration: 2,
-            //     rotation: 360,
-            // })
-
-            // const gsap_red = gsap.timeline({
-            //     scrollTrigger: {
-            //         trigger: '.gsap_red',
-            //         toggleActions: 'restart pause reverse pause',
-            //     },
-            // })
-
-            // gsap_red
-            //     .to('.gsap_red', {
-            //         duration: 1,
-            //         backgroundColor: '#FFA500',
-            //         ease: 'none',
-            //     })
-            //     .to('.gsap_red p', {
-            //         scale: 2,
-            //         repeat: -1,
-            //         yoyo: true,
-            //         ease: 'power2',
-            //     })
-
-            // const yoyo = gsap.timeline({
-            //     scrollTrigger: '.yoyo',
-            // })
-
-            // yoyo.to('.yoyo p', {
-            //     scale: 2,
-            //     repeat: -1,
-            //     yoyo: true,
-            //     ease: 'power2',
-            // })
         }, scrollPage) // Scope selector
         return () => ctx.revert() // cleanup
-    }, [])
+    }, [scrollPage.current])
 
     return (
         <main ref={scrollPage} className="homepage-container" id="pin-windmill">
-            <h1>Pink Panther</h1>
+            {/* <h1>Pink Panther</h1> */}
             <NavComponent />
             <CardSectionList />
             <SpinnerIcon className="spinner-svg" id="pin-windmill-svg" />
