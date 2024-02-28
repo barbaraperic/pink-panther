3225620adfcc374872a05c5baad9ab6a20aefd86
Barbara Peric <b.peric10@gmail.com>
2024-02-28 14:23:05 +0000

    scroll effect on nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,7 +1,10 @@
 'use client'
-import SpinnerIcon from '../icons/Spinner'
 import styles from './Nav.module.css'
-import { useGSAP } from '@gsap/react'
+import { gsap, Power3 } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
 export default function NavComponent() {
     const links = [
@@ -19,45 +22,23 @@ export default function NavComponent() {
         },
     ]
 
-    // const { contextSafe } = useGSAP({ scope: container })
-
-    // useGSAP(
-    //     () => {
-    //         tl.current = gsap
-    //             .timeline({ repeat: -1, yoyo: true }) // Repeat indefinitely and yoyo back and forth
-    //             .to('.candles', {
-    //                 stagger: 1.2,
-    //                 scaleY: gsap.utils.random(0.9, 1.1), // Random scale on the Y-axis
-    //                 duration: gsap.utils.random(0.1, 0.5), // Random duration for the flicker
-    //                 onStart: function () {
-    //                     gsap.set('.candles', {
-    //                         fill: '#FFA500',
-    //                         opacity: Math.random() + 0.5,
-    //                         transitionProperty: 'all',
-    //                         transitionTimingFunction:
-    //                             'cubic-bezier(0.4, 0, 0.2, 1)',
-    //                         transitionDuration: '150ms',
-    //                     })
-    //                 },
-    //                 ease: 'none', // Linear easing for a more natural flicker
-    //             })
-    //     },
-    //     { scope: container }
-    // )
-
-    // gsap.timeline({
-    //     scrollTrigger: {
-    //         scrub: 1,
-    //         trigger: '.scroll-trigger-ready__worm-wrap',
-    //         start: 'top 90%',
-    //         end: 'bottom 30%',
-    //     },
-    // })
-
     function handleClick(e: any) {
-        const href = e.target.href
-        const topY = e.target.getBoundingClientRect().y
-        console.log(topY)
+        e.preventDefault()
+        const href = e.target.getAttribute('href')
+        const targetElement = document.querySelector(href)
+
+        if (targetElement) {
+            const topY =
+                targetElement.getBoundingClientRect().top + window.scrollY
+            gsap.to(window, {
+                scrollTo: {
+                    y: topY,
+                    autoKill: false,
+                },
+                duration: 1.3,
+                ease: Power3.easeOut,
+            })
+        }
     }
 
     return (
