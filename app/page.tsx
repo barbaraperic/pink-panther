ca1aadcfcee1bd898c50ac35029d14df389cb94b
Barbara Peric <b.peric10@gmail.com>
2024-03-14 18:49:37 +0100

    new landing page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import useAnimateScroll from '@/hooks/useAnimateScroll'
 import IntroTitleComponent from './components/IntroTitle'
 import BackgroundLayoutComponent from './components/BackgroundLayout'
 import styles from './homepage.module.css'
+import DetectiveIllustration from './illustrations/Detective'
+import LogoIcon from './icons/Logo'
 
 export default function Home() {
     const scrollPage = useRef(null)
@@ -21,13 +23,25 @@ export default function Home() {
 
     return (
         <div className={styles.container} ref={scrollPage} id="pin-windmill">
-            <NavComponent />
-            <CardSectionList />
-            {/* <SpinnerIcon
-                className={styles['spinner-svg']}
-                id="pin-windmill-svg"
-            /> */}
-            <div id="pin-windmill-wrap"></div>
+            <nav className={styles['nav-element']}>
+                <div className={styles.nav}></div>
+                <LogoIcon />
+            </nav>
+            {/* <div className={styles.title}>
+                <IntroTitleComponent />
+            </div> */}
+            <div className={styles['main-grid']}>
+                <NavComponent />
+                {/* <CardSectionList /> */}
+                <SpinnerIcon
+                    className={styles['spinner-svg']}
+                    id="pin-windmill-svg"
+                />
+                <div id="pin-windmill-wrap"></div>
+            </div>
+            {/* <div className={styles['illustration-grey']}>
+                <DetectiveIllustration />
+            </div> */}
         </div>
     )
 }
